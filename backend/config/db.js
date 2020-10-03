be55efebb582e04f8c211f78a7e227cdfbfe2a10
Ersoy Efe Uruk <efeuruk1@gmail.com>
2020-10-03 18:20:59 +0300

    Color.js abd Models are added

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+// DB config file
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+			useCreateIndex: true,
+		});
+
+		console.log(
+			`MongoDB Connected: ${conn.connection.host}`.cyan.underline
+		);
+	} catch (error) {
+		// Exit with error
+		console.error(`Error: ${error.message}`.red.underline.bold);
+		process.exit(1);
+	}
+};
+
+export default connectDB;
