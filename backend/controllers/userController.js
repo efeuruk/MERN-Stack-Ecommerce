928b7948a5ba6dbd6213a0012b054a757054f292
Ersoy Efe Uruk <efeuruk1@gmail.com>
2020-10-15 09:53:42 +0300

    Working with jwt for auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
-import UserModel from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc  Auth user & get token
 // @route POST /api/users/login
@@ -16,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
 			name: user.name,
 			email: user.email,
 			isAdmin: user.isAdmin,
-			token: null,
+			token: generateToken(user._id),
 		});
 	} else {
 		res.status(401);
@@ -24,4 +24,22 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser };
+// @desc  Get user profile
+// @route GET /api/users/profile
+// @acess Private
+const getUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		res.status(404);
+		throw new Error('User not found');
+	}
+});
+
+export { authUser, getUserProfile };
